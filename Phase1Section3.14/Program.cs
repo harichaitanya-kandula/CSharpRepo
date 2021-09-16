ae130f5e909e5cc82ea5866de265bf8fbc63ee7c
Harichaitanya Kandula <Hari_Kandula@Dell.com>
2021-09-16 15:14:14 -0500

    ReadFileProject

---
+++ Phase1Section3.14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Phase1Section3._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            readStudentInfo();
+        }
+
+        public static void readStudentInfo()
+        {
+          
+            string filename = "C:\\FSDTraining\\CSharpRepo\\Phase1Section3.14\\StudentData.txt";
+
+            if (File.Exists(filename))
+            {
+                Console.WriteLine(filename + " exists");
+                string[] lines = File.ReadAllLines(filename);
+                foreach (string s in lines)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+            else
+                Console.WriteLine(filename + " does not exist");
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # CSharpRepo
+
+Project Description:
+Write a program that will retrieve student data from a text file and display it.
+
+Steps:
+1. Create Console C# Application Project
+2. Create StudentData txt file with student data
+3. import System.IO
+4. Create readStudentInfo Method that varifies if provided file
+   exists and if exists, prints filename and reads each line in the file and prints on the    console.
+5. Call newly created method in Main Method
+6. Build the solution 
+7. Run the project with Ctrl+F5
+
+
+Output:
+C:\FSDTraining\CSharpRepo\Phase1Section3.14\StudentData.txt exists
+Name: Emily Smith
+Age: 10
+Gender:Female
+Standard: 5
+Name: tom Jones
+Age: 12
+Gender:Male
+Standard: 7
+Name: Tony Seb
+Age: 10
+Gender:Male
+Standard: 5
